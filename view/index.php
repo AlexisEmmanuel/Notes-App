22a498eda505f790d82392ca69346345451fb318
AlexisBR <soyalexisemmanuel@gmail.com>
2022-07-10 17:03:43 -0500

    Fixed

--- view/index.php
+++ view/index.php
@@ -35,28 +35,11 @@
                 <p class="card-text"><?php echo $note['content_note']; ?></p>
                 <div class="">
                   <a href="?v=editNote&id=<?php echo $note['id_note']; ?>" class="btn btn-primary">Edit card</a>
-                  <button data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-danger">Delete card</button>
+                  <a href="?v=deleteNote&id=<?php echo $note['id_note']; ?>" type="button" class="btn btn-danger">Delete</a>
                 </div>
               </div>
             </div>
           <?php } ?>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Confirmation</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          Are you sure you want to delete the note?
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Do not delete</button>
-          <a href="?v=deleteNote&id=<?php echo $note['id_note']; ?>" type="button" class="btn btn-danger">Delete</a>
         </div>
       </div>
     </div>
